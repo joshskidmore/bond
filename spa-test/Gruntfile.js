5cb1adac7b3caf058785f68af87c361f5ff6187c
Josh Skidmore <josh@josh.sc>
2013-09-05 18:31:47 -0400

    Adding onto spa-test

--- spa-test/Gruntfile.js
+++ spa-test/Gruntfile.js
@@ -12,11 +12,24 @@ module.exports = function(grunt) {
 					'index.html': 'views/index.jade'
 				}
 			}
+		},
+		less: {
+			compile: {
+				options: {
+					data: {
+						debug: true
+					}
+				},
+				files: {
+					'bond.css': 'less/bond.less'
+				}
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jade');
+	grunt.loadNpmTasks('grunt-contrib-less');
 
-	grunt.registerTask('default', ['jade']);
+	grunt.registerTask('default', ['jade','less']);
 	
 };
---
+++ spa-test/lib/index.js
@@ -0,0 +1,2 @@
+
+exports.services = require('./services');
