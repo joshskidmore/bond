e6c8c8a0ce370f933da542594840d093f9b93ca3
Jeremy Martin <jmar777@gmail.com>
2013-09-06 13:43:59 -0400

    ctrl+p toggles side menu

--- spa-test/Gruntfile.js
+++ spa-test/Gruntfile.js
@@ -32,7 +32,8 @@ module.exports = function(grunt) {
 					separator: ';'
 				},
 				src: [
-					'public/src/js/jquery-2.0.3.js',
+					'public/src/js/third-party/jquery-2.0.3.js',
+					'public/src/js/third-party/mousetrap.min.js',
 					'public/src/js/side-menu.js'
 				],
 				dest: 'public/js/bond.js'
--- spa-test/public/src/js/side-menu.js
+++ spa-test/public/src/js/side-menu.js
@@ -1,6 +1,10 @@
 $(function() {
-	$('#nav-toggle').click(function(e) {
+	var toggleSideMenu = function(e) {
 		e.preventDefault();
 		$('body').toggleClass('side-menu-expanded');
-	});
+	};
+
+	$('#nav-toggle').click(toggleSideMenu);
+	Mousetrap.bind('ctrl+p', toggleSideMenu);
+	
 });
