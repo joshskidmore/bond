71a816f1cd28984e05c0ed3615c11007dc7cb188
Jeremy Martin <jmar777@gmail.com>
2013-09-06 10:39:15 -0400

    fancier bootstrap integration, resource bundling, and move all the things for fun

--- spa-test/Gruntfile.js
+++ spa-test/Gruntfile.js
@@ -16,20 +16,46 @@ module.exports = function(grunt) {
 		less: {
 			compile: {
 				options: {
+					compress: true,
 					data: {
 						debug: true
 					}
 				},
 				files: {
-					'public/css/bond.css': 'less/bond.less'
+					'public/css/bond.css': 'public/src/less/bond.less'
 				}
+			}
+		},
+		concat: {
+			scripts: {
+				options: {
+					separator: ';'
+				},
+				src: [
+					'public/src/js/jquery-2.0.3.js'
+				],
+				dest: 'public/js/bond.js'
+			}
+		},
+		copy: {
+			fonts: {
+				files: [
+					{
+						expand: true,
+						src: ['public/src/bootstrap/fonts/*'],
+						dest: 'public/fonts/',
+						flatten: true
+					}
+				]
 			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jade');
 	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 
-	grunt.registerTask('default', ['jade','less']);
+	grunt.registerTask('default', ['jade', 'less', 'concat', 'copy']);
 	
 };
