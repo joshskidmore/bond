fa1f5c7ccbd46a36f34c0018b3c6dccdddbf885d
Josh Skidmore <josh@josh.sc>
2013-09-08 17:15:40 -0400

    Updating packaging script for faster OS X startup

--- spa-test/scripts/package-osx.sh
+++ spa-test/scripts/package-osx.sh
@@ -3,9 +3,6 @@
 echo "Removing old Bond.app..."
 rm -rf Bond.app
 
-echo "Creating nw zip bundle..."
-zip -r ../app.nw *
-
 echo "Creating temp directory..."
 mkdir Bond.app
 
@@ -13,7 +10,7 @@ echo "Copying app binaries..."
 rsync -ravP ../node-webkit-v0.7.2-osx-ia32/node-webkit.app/ ./Bond.app/
 rsync -ravP scripts/packaging-osx/ ./Bond.app/
 
-echo "Moving app.nw..."
-mv ../app.nw ./Bond.app/Contents/Resources/
+echo "Copying app.nw..."
+rsync -ravP ./ ./Bond.app/Contents/Resources/app.nw
 
 echo "Finished - the app is ./Bond.app"
