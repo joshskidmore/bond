b8baa254f25fe75aa18132d2cb18a757dca3a832
Josh Skidmore <josh@josh.sc>
2013-09-15 12:26:26 -0400

    Basic accounts controller/partial + route handler

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,6 @@ var bond = angular.module('bond', [])
 		$routeProvider
 			.when('/', { templateUrl: 'partials/chat.html', controller: ChatCtrl })
 			.when('/settings', { templateUrl: 'partials/settings.html', controller: SettingsCtrl })
+			.when('/accounts', { templateUrl: 'partials/accounts.html', controller: AccountsCtrl })
 			.otherwise({ redirectTo: '/' });
 	});
