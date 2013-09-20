0a98e3a9781c5f6d82b2d13cb0a75aa31aa99f77
Jeremy Martin <jmar777@gmail.com>
2013-09-20 17:05:37 -0400

    restoring some of Josh's work that I reset

--- scripts/install-nw-osx.sh
+++ scripts/install-nw-osx.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Downloading + Installing node-webkit for OS X..."
-wget --no-check-certificate https://s3.amazonaws.com/node-webkit/v0.7.2/node-webkit-v0.7.2-osx-ia32.zip
-unzip node-webkit-v0.7.2-osx-ia32.zip -d node-webkit-v0.7.2-osx-ia32 && rm node-webkit-v0.7.2-osx-ia32.zip
+curl -O https://s3.amazonaws.com/node-webkit/v0.7.2/node-webkit-v0.7.2-osx-ia32.zip
+unzip node-webkit-v0.7.2-osx-ia32.zip -d node-webkit-v0.7.2-osx-ia32
+mv node-webkit-v0.7.2-osx-ia32/node-webkit.app /Applications
+rm node-webkit-v0.7.2-osx-ia32.zip
+rm -rf node-webkit-v0.7.2-osx-ia32
