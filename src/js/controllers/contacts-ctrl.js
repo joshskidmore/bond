6e9c7acfae93c431a17de31d6ce304e77f07b290
Jeremy Martin <jmar777@gmail.com>
2013-10-06 19:19:03 -0400

    custom sort function for contacts repeater (orders by state and then jid)

--- src/js/controllers/contacts-ctrl.js
+++ src/js/controllers/contacts-ctrl.js
@@ -1,5 +1,29 @@
 function ContactsCtrl($scope, contact) {
-	// $scope.accounts = accounts.accounts;
 	$scope.contacts = contact.contacts;
-	// $scope.contacts = contact.getOnlineContacts();
+	
+	$scope.stateAndNameSortValue = function(contact) {
+		var sortVal = '';
+		switch (contact.state) {
+			case 'online':
+				sortVal += 'A';
+				break;
+			case 'away':
+				sortVal += 'B';
+				break;
+			case 'dnd':
+				sortVal += 'C';
+				break;
+			case 'offline':
+				sortVal += 'D';
+				break;
+			default:
+				// shouldn't really ever get here...
+				sortVal += 'E';
+				break;
+		}
+
+		sortVal += contact.jid;
+
+		return sortVal;
+	};
 }
