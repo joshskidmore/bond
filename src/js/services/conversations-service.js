a58ef85b5d84657867b610a594f84260f7af177c
Jeremy Martin <jmar777@gmail.com>
2013-10-31 15:59:49 -0400

    wrap conversations service in a IIFE

--- src/js/services/conversations-service.js
+++ src/js/services/conversations-service.js
@@ -1,53 +1,55 @@
-var EventEmitter = require('events').EventEmitter,
-	util = require('util');
+(function() {
+	var EventEmitter = require('events').EventEmitter,
+		util = require('util');
 
-function ConversationsService(accounts, contact) {
-	EventEmitter.call(this);
+	function ConversationsService(accounts, contact) {
+		EventEmitter.call(this);
 
-	this.contactService = contact;
+		this.contactService = contact;
 
-	this.conversations = [];
+		this.conversations = [];
 
-	accounts.on('chat', this.handleChatEvent.bind(this));
-}
-util.inherits(ConversationsService, EventEmitter);
-
-ConversationsService.prototype.handleChatEvent = function(event) {
-	var conversation = this.getConversation(event.data.jid, event.accountJid);
-
-	// todo: this probably means we didn't recognize the jid. What do we do here?
-	if (!conversation) return;
-
-	conversation.messages.push(event.data.message);
-
-	this.emit('chat');
-};
-
-ConversationsService.prototype.getConversation = function(fromJid, toJid) {
-	for (var i = 0, len = this.conversations.length; i < len; i++) {
-		var conversation = this.conversations[i];
-
-		if (conversation.from.jid === fromJid && conversation.accountJid === toJid) {
-			return conversation;
-		}
+		accounts.on('chat', this.handleChatEvent.bind(this));
 	}
+	util.inherits(ConversationsService, EventEmitter);
 
-	var contact = this.contactService.getContact(fromJid);
+	ConversationsService.prototype.handleChatEvent = function(event) {
+		var conversation = this.getConversation(event.data.jid, event.accountJid);
 
-	if (!contact) {
-		return console.log('Received message from unknown jid: ', fromJid);
-	}
+		// todo: this probably means we didn't recognize the jid. What do we do here?
+		if (!conversation) return;
 
-	var newConversation = {
-		accountJid: toJid,
-		from: contact,
-		messages: []
+		conversation.messages.push(event.data.message);
+
+		this.emit('chat');
 	};
-	this.conversations.push(newConversation);
 
-	this.emit('conversation', newConversation);
+	ConversationsService.prototype.getConversation = function(fromJid, toJid) {
+		for (var i = 0, len = this.conversations.length; i < len; i++) {
+			var conversation = this.conversations[i];
 
-	return newConversation;
-};
+			if (conversation.from.jid === fromJid && conversation.accountJid === toJid) {
+				return conversation;
+			}
+		}
 
-bond.service('conversations', ConversationsService);
+		var contact = this.contactService.getContact(fromJid);
+
+		if (!contact) {
+			return console.log('Received message from unknown jid: ', fromJid);
+		}
+
+		var newConversation = {
+			accountJid: toJid,
+			from: contact,
+			messages: []
+		};
+		this.conversations.push(newConversation);
+
+		this.emit('conversation', newConversation);
+
+		return newConversation;
+	};
+
+	bond.service('conversations', ConversationsService);
+})();
